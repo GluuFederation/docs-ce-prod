8b945f4f443efd3e32d9ad0bf540b97da0d1951d
jschristie <jschristiein@gmail.com>
2017-04-18 23:00:30 +0530

    updated site & - modified repo url

--- 2.4.4/update-site.sh
+++ 2.4.4/update-site.sh
@@ -2,11 +2,12 @@
 git pull
 mkdocs build
 rm -rf docs/
-mv site/ docs/
+rm -rf 2.4.4/
+mv site/ 2.4.4/
 echo -n "Enter task Performed >"
 read text
 echo "Entered Task: $text"
 
 git add -A
 git commit -m "updated site & - $text"
-git push origin master
+git push origin 2.4.4
