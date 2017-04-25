6787697be38fedd0a701c67138fc00d544406b9d
jschristie <jschristiein@gmail.com>
2017-04-25 23:43:01 +0530

    updated site & - modified script

---
+++ 3.0.2/update-site-local.sh
@@ -0,0 +1,13 @@
+#! /bin/bash
+git pull origin 3.0.2
+mkdocs build
+rm -rf latest/
+rm -rf 3.0.2/
+mv site/ 3.0.2/
+echo -n "Enter task Performed >"
+read text
+echo "Entered Task: $text"
+
+git add -A
+git commit -m "updated site & - $text"
+git push origin 3.0.2
