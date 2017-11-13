33323c3c3daedbae86d02b1aa8d801bd9c85a7e3
jschristie <jschristiein@gmail.com>
2017-11-13 22:49:44 +0530

    updated site & - initial Commit

--- 3.1.2/update-site-local.sh
+++ 3.1.2/update-site-local.sh
@@ -1,9 +1,9 @@
 #! /bin/bash
-git pull origin 3.1.1
+git pull origin 3.1.2
 echo -n "Enter task Performed >"
 read text
 echo "Entered Task: $text"
 
 git add -A
 git commit -m "updated site & - $text"
-git push origin 3.1.1
+git push origin 3.1.2
