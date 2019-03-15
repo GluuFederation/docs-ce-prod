c3fe8aeabf95aae16d8a526780f11fa79436869e
Sam Morris <36208047+shmorri@users.noreply.github.com>
2019-03-15 12:58:33 -0500

    Replaced with current dynamic script

--- docs/source/admin-guide/sample-dynamic-script.py
+++ docs/source/admin-guide/sample-dynamic-script.py
@@ -5,6 +5,8 @@
 #
 
 from org.xdi.model.custom.script.type.scope import DynamicScopeType
+from org.xdi.service.cdi.util import CdiUtil
+from org.xdi.oxauth.service import UserService
 from org.xdi.util import StringHelper, ArrayHelper
 from java.util import Arrays, ArrayList
 
@@ -15,44 +17,37 @@ class DynamicScope(DynamicScopeType):
         self.currentTimeMillis = currentTimeMillis
 
     def init(self, configurationAttributes):
-        print "Dynamic scope. Initialization"
+        print "Permission dynamic scope. Initialization"
 
-        print "Dynamic scope. Initialized successfully"
+        print "Permission dynamic scope. Initialized successfully"
 
         return True   
 
     def destroy(self, configurationAttributes):
-        print "Dynamic scope. Destroy"
-        print "Dynamic scope. Destroyed successfully"
+        print "Permission dynamic scope. Destroy"
+        print "Permission dynamic scope. Destroyed successfully"
         return True   
 
-    # Update Json Web token before signing/encrypting it
+    # Update Json Web token before signing/encrypring it
     #   dynamicScopeContext is org.xdi.oxauth.service.external.context.DynamicScopeExternalContext
     #   configurationAttributes is java.util.Map<String, SimpleCustomProperty>
     def update(self, dynamicScopeContext, configurationAttributes):
-        print "Dynamic scope. Update method"
+        print "Permission dynamic scope scope. Update method"
 
-        dynamicScopes = dynamicScopeContext.getDynamicScopes()
+        authorizationGrant = dynamicScopeContext.getAuthorizationGrant()
         user = dynamicScopeContext.getUser()
-        jsonToken = dynamicScopeContext.getJsonToken()
-        claims = jsonToken.getClaims()
+        jsonWebResponse = dynamicScopeContext.getJsonWebResponse()
+        claims = jsonWebResponse.getClaims()
 
-        # Iterate through list of dynamic scopes in order to add custom scopes if needed
-        print "Dynamic scope. Dynamic scopes:", dynamicScopes
-        for dynamicScope in dynamicScopes:
-            # Add organization name if there is scope = org_name
-            if (StringHelper.equalsIgnoreCase(dynamicScope, "org_name")):
-                claims.setClaim("org_name", "Gluu, Inc.")
-                continue
-
-            # Add work phone if there is scope = work_phone
-            if (StringHelper.equalsIgnoreCase(dynamicScope, "work_phone")):
-                workPhone = user.getAttribute("telephoneNumber");
-                if (StringHelper.isNotEmpty(workPhone)):
-                    claims.setClaim("work_phone", workPhone)
-                continue
+        userService = CdiUtil.bean(UserService)
+        roles = userService.getCustomAttribute(user, "role")
+        if roles != None:
+            claims.setClaim("role", roles.getValues())
 
         return True
 
+    def getSupportedClaims(self, configurationAttributes):
+        return Arrays.asList("role")
+
     def getApiVersion(self):
-        return 1
+        return 2
